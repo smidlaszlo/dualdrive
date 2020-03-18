de1589532dd6cb43a9dfdd0cf34cb4c5ef567328
smidlaszlo <smid@iit.uni-miskolc.hu>
2020-03-18 17:20:26 +0100

    sysout

---
+++ dualdrive_project/src/dualdrive_project/program.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package dualdrive_project;
+
+/**
+ * @author smid
+ *
+ */
+public class program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("dual drive");
+	}
+
+}
